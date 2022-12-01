16714c6af61fdc0392e22aabae6178ab4830f09a
Akkenton <akkenton.msk@gmail.com>
2022-12-01 23:09:43 +0300

    Решена задача №27

--- task27/Program.cs
+++ task27/Program.cs
@@ -6,6 +6,28 @@
 
 //  ----------------------------------- START
 
+int AddNumber(string str)
+{
+    Console.WriteLine(str);
+    int num = int.Parse(Console.ReadLine());
+    return num;
+}
 
+int SummOfNumbers(int num)
+{
+    int result = 0;
+    while (num != 0)
+    {
+        result = result + num % 10;
+        num = num / 10;
+    }
+    return result;
+}
+
+// ---------- Исполнение ----------------
+
+int num = AddNumber("Введите число: ");
+int result = SummOfNumbers(num);
+Console.WriteLine($"Сумма составных чисел числа {num} равна {result}");
 
 //  --------------------------------------END
